9ed5c2d170cbdd092ae055844c6dac27b8bd25b9
ryan25-girsang1 <ryangirsang25@gmail.com>
2026-05-08 23:20:24 +0700

    Inisiasi halaman login

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.welcome');
-}) ->name('welcome'); 
+})->name('welcome');
 
 Route::get('/detail-loker', function () {
     return view('pages.detail_loker');
