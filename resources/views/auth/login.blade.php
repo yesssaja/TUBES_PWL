eef81247ebee2c0876c701181d28ff9c371c5b17
ryan25-girsang1 <ryangirsang25@gmail.com>
2026-05-10 00:32:26 +0700

    mendesain login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,25 @@
 <x-guest-layout>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap');
+    
+        body {
+            font-family: 'Oswald',sans-serif;
+            background-color: #f6b333;
+        }
+
+        .text-custom-red{
+            color: #d12026;
+        }
+
+        .border-custom-red{
+            border-color: #d12026;
+        }
+
+        .bg-custom-red{
+            background-color: #d12026;
+        }
+        </style>
+
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -7,16 +28,16 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-label for="email" :value="__('Email')" class=" text-custom-red font-bold uppercase tracking-widest text-sm"/>
+            <x-text-input id="email" class="block mt-1 w-full border-2 border-custom-red rounded-xl font-bold bg-gray-50" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Password')" class="text-custom-red font-bold uppercase tracking-widest text-sm"/>
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block mt-1 w-full border-2 border-custom-red rounded-xl font-bold bg-gray-50"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
@@ -28,18 +49,18 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="text-custom-red font-bold uppercase text-sm">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="text-custom-red font-bold uppercase text-sm" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="bg-custom-red text-white font-bold uppercase tracking-widest rounded-xl">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
