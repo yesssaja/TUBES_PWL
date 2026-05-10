4dccc887afbb823d4cb70ed0149c9b84c1dbc797
jelialvonzolaia-sys <alvonzolaia@gmail.com>
2026-05-10 22:13:10 +0700

    menambahkan halaman lamaran dan tampilan sukses setelah submit data

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,19 @@ Route::get('/detail-loker', function () {
 Route::get('/event', function () {
     return view('pages.event');
 });
+
+Route::get('/lamaran', function () {
+    return view('pages.lamaran');
+});
+
+Route::get('/success', function () {
+
+    return view('pages.success');
+
+});
+
+Route::post('/lamaran-submit', function () {
+
+    return redirect('/success');
+
+});
