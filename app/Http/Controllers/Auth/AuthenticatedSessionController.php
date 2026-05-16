b688b2a3dc7c36c65098c737ec9f1129742465d2
ryan25-girsang1 <ryangirsang25@gmail.com>
2026-05-16 13:52:39 +0700

    tambahkan logika akun admin ke halaman admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -25,10 +25,13 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
-
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        if(Auth::user()->role === 'admin'){
+            return redirect()->intended('/admin');
+            }
+
+        return redirect()->intended('/');
     }
 
     /**
