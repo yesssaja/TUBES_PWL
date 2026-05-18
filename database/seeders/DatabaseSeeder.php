296d7f9782d7d14522c7e0cb7d6bd2336e411d82
jelialvonzolaia-sys <alvonzolaia@gmail.com>
2026-05-18 15:23:54 +0700

    database seader

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,14 +11,14 @@ class DatabaseSeeder extends Seeder
 
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
-        $this->call([
-            ServiceSeeder::class,
-        ]);
+        // $this->call([
+        //     ServiceSeeder::class,
+        // ]);
 
        $this->call([
     UserSeeder::class,
