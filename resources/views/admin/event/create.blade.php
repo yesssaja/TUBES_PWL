980a2402675cb1478472e9b0e3a83a4d0ce43d7d
yezicaj-byte <yezicajj@gmail.com>
2026-05-20 10:52:50 +0700

    jam d db event

--- resources/views/admin/event/create.blade.php
+++ resources/views/admin/event/create.blade.php
@@ -56,6 +56,17 @@
 
             </div>
 
+            <div class="mb-4">
+               <label class="block font-semibold mb-2">
+                   Jam Event
+               </label>
+           
+               <input type="time"
+                      name="jam"
+                      value="{{ old('jam', isset($event) ? substr($event->jam, 0, 5) : '') }}"
+                      class="w-full border rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-red-400">
+            </div>
+            
             <!-- Lokasi -->
             <div class="mb-5">
 
