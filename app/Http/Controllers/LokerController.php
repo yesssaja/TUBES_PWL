a4e1fa72c37c06822d59e52a5dc69fb2c926345d
jelialvonzolaia-sys <alvonzolaia@gmail.com>
2026-06-02 10:09:21 +0700

    rapikan folder loker

--- app/Http/Controllers/LokerController.php
+++ app/Http/Controllers/LokerController.php
@@ -14,13 +14,13 @@ class LokerController extends Controller
             ->latest()
             ->get();
 
-        return view('pages.loker', compact('lokers'));
+        return view('users.loker.dashboard.loker', compact('lokers'));
     }
 
     public function show(Loker $loker)
     {
         $loker->load('perusahaan');
 
-        return view('pages.detail_loker', compact('loker'));
+        return view('users.loker.detail.detail_loker', compact('loker'));
     }
 }
