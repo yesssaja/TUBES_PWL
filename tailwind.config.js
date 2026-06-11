0b2729504fadf56fb87d97b97eebee70c60a785f
yezicaj-byte <yezicajj@gmail.com>
2026-06-11 12:37:56 +0700

    taiilwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
-    "./resources/**/*.jsx",
+    "./resources/**/*.vue",
   ],
   theme: {
     extend: {},
